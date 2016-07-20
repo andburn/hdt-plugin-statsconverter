16c5fe06c841c91e3ee53be1190b9af1c73e00a7
Andrew Burnett <andrew@andburn.info>
2016-07-20 20:53:15 +0100

    Add icon and text button control
    and setup MVVM structure

---
+++ StatsConverter/ViewModels/ImportViewModel.cs
@@ -0,0 +1,8 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace HDT.Plugins.StatsConverter.ViewModels
+{
+	public class ImportViewModel : ViewModelBase
+	{
+	}
+}
---
+++ StatsConverter/ViewModels/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Hearthstone_Deck_Tracker.Utility.Logging;
+
+namespace HDT.Plugins.StatsConverter.ViewModels
+{
+	public class MainViewModel : ViewModelBase
+	{
+		private Dictionary<string, ViewModelBase> _viewModels = new Dictionary<string, ViewModelBase> {
+			{ "settings", new SettingsViewModel() },
+			{ "import", new ImportViewModel() },
+			{ "export", new ExportViewModel() }
+		};
+
+		private string _contentTitle;
+
+		public string ContentTitle
+		{
+			get { return _contentTitle; }
+			set { Set(() => ContentTitle, ref _contentTitle, value); }
+		}
+
+		private ViewModelBase _contentViewModel;
+
+		public ViewModelBase ContentViewModel
+		{
+			get { return _contentViewModel; }
+			set { Set(() => ContentViewModel, ref _contentViewModel, value); }
+		}
+
+		public RelayCommand<string> NavigateCommand { get; private set; }
+
+		public MainViewModel()
+		{
+			ContentTitle = "Settings";
+			NavigateCommand = new RelayCommand<string>(x => OnNavigation(x));
+		}
+
+		private void OnNavigation(string location)
+		{
+			Log.Debug($"onNav({location})");
+			// TODO do nothing if the same as current?
+			if (_viewModels.ContainsKey(location.ToLower()))
+			{
+				Log.Debug("Found key");
+				ContentViewModel = _viewModels[location.ToLower()];
+			}
+		}
+	}
+}
